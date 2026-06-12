8a28f0896bbbea3b3388c98a7046fe30bbc108b3
xKurty06 <zeankurtbalboa06@gmail.com>
2026-06-12 16:04:47 +0800

    fix: update bodySizeLimit in serverActions to 3mb

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     serverActions: {
-      bodySizeLimit: "96mb",
+      bodySizeLimit: "3mb",
     },
   },
   images: {
